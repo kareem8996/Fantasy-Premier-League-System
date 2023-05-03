34a44b4aa9b19aac3fe02fa05dd5e0fa40d21caf
Kareem8996 <kareem8996@hotmail.com>
2023-05-03 23:06:23 +0300

    - Added Extra Attributes for player in gameWeek Object. Thus, adding setters, gettters, updating Calculate Points and changing readPlayer.
    Added attribute in User_Team called totalPointsPerWeek which is a vector of integers that stores points collected by user each week. Additionally added setters, getters, and update functions respectively.
    
    - Added function getTotalPoints for UserTeam object which calls calculate Points from each player in the team and adds them up.

--- Data Structures/Attacker.cpp
+++ Data Structures/Attacker.cpp
@@ -9,5 +9,12 @@ int Attacker::CalculatePoints() {
 	points += this->getPlayer_History().back().getAssists_gameweek() * 3;
 	points -= this->getPlayer_History().back().getYellow_cards_gameweek();
 	points -= this->getPlayer_History().back().getRed_cards_gameweek()*3;
+
+	if (this->getPlayer_History().back().getMinutesPlayed()< 60) points += 1;
+	else points += 2;
+
+	points -= this->getPlayer_History().back().getPenaltiesMissed() * 2;
+	points += this->getPlayer_History().back().getBonus();
+	points -= this->getPlayer_History().back().getOwnGoals() * 2;
 	return points;
 }
