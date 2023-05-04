3ffa94947b236bda461e2f1d0fc3b7b78913cc88
Kareem8996 <kareem8996@hotmail.com>
2023-05-04 13:29:09 +0300

    Added Read Club

--- Data Structures/Main.cpp
+++ Data Structures/Main.cpp
@@ -5,8 +5,16 @@ int main()
 
     
     /*System::RunSys();*/
+    System::readClub();
     System::readPlayers();
+    for (auto& it : System::AllClubs) {
+        // Do stuff
+        cout << it.first<<"\t";
+        cout << it.second->getLeaguePoints() << endl;
+        for (auto& player : it.second->getSquad()) {
+            cout << player.second->getFullname() << endl;
+        }
+    }
     System::RunSys();
-    
     
 }
